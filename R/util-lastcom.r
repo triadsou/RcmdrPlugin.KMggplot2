7d3be03a2eab64c251ef4dc98ed1694103055a64
Triad sou <triadsou@gmail.com>
2016-12-19 23:20:56 +0900

    New function: list the last command.

--- R/util-lastcom.r
+++ R/util-lastcom.r
@@ -1,4 +1,10 @@
-
+#' Listing the last command
+#'
+#' This function lists the last command is executed by \code{RcmdrPlugin.KMggplot2}
+#'
+#' @rdname util-lastcom
+#' @keywords documentation
+#' @export
 lastcom <- function() {
   
   cat(.lastcom)
